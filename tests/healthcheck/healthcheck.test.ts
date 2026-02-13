10aee77a2191de3346cb9f163fda2a6545602bee
HonzaPfeiffer <jan.pfeiffer@seznam.cz>
2026-02-13 15:04:24 +0100

    fix: update page title in tests

--- tests/healthcheck/healthcheck.test.ts
+++ tests/healthcheck/healthcheck.test.ts
@@ -2,5 +2,5 @@ import { test, expect } from '@playwright/test';
 
 test('homepage loads', async ({ page }) => {
   await page.goto('/');
-  await expect(page).toHaveTitle(/Login/);
+  await expect(page).toHaveTitle(/Invoicer/);
 });
